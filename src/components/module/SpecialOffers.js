7688bfb6824b708a6aaf2e4af4a390efc1b42d23
Amir Hossin Sharifi Fard <amirawade7@gmail.com>
2025-05-03 17:06:10 +0330

    home page is complited

--- src/components/module/SpecialOffers.js
+++ src/components/module/SpecialOffers.js
@@ -4,12 +4,12 @@ const SpecialOffers = () => {
   return (
     <div className='md:bg-[#fff] bg-[#e5e5e5]'>
       <div className='max-w-[1200px] m-auto'>
-        <div className='md:hidden flex justify-between items-center m-auto bg-[#ffe0d0] border border-[#d53129] p-4 rounded-lg mb-4 text-[#f4561b]'>
+        <div className=' flex justify-between md:justify-center  items-center m-auto md:bg-[#ffe0d0] border border-[#d53129] md:border-none p-4 rounded-lg mb-4 text-[#f4561b]'>
           <h3 className='font-bold'>پیشنهاد ویژه لیپک</h3>
-          <h3>button</h3>
+          <h3 className="md:hidden">button</h3>
         </div>
 
-        <div className='flex  gap-2 md:justify-between content-center flex-wrap m-auto md:divide-x-2 divide-[rgba(0,0,0,.1)] px-[1.5rem] max-md:justify-evenly max-md:content-center'>
+        <div className='flex  gap-2 md:justify-between content-center flex-wrap m-auto  px-[1.5rem] max-md:justify-evenly max-md:content-center'>
           <CardSpecialOffer />
           <CardSpecialOffer />
           <CardSpecialOffer />
--- src/components/template/HomePage.js
+++ src/components/template/HomePage.js
@@ -2,15 +2,17 @@ import AmazingOffers from "../module/AmazingOffers";
 import Category from "../module/Category";
 import Details from "../module/Details";
 import SpecialOffers from "../module/SpecialOffers";
+import Accordion from "../module/Accordion";
 
 const HomePage = () => {
   return (
     <div dir='rtl' className='mx-auto bg-[#e5e5e5]  '>
-      {/* <AmazingOffers /> */}
-      {/* <SpecialOffers /> */}
+      <AmazingOffers />
+      <SpecialOffers />
       <div className='bg-[#fff]'>
-        {/* <Category /> */}
+        <Category />
         <Details />
+        <Accordion reverse={true} />
       </div>
     </div>
   );
